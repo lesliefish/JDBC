6bcdb15bfe406da8a1653eda741512c87ad68b18
leiyu <yuleics@outlook.com>
2019-03-10 17:33:10 +0800

    JDBC test first demo

---
+++ src/com/lesliefish/Main.java
@@ -0,0 +1,10 @@
+package com.lesliefish;
+
+import com.lesliefish.test01firstexample.FirstExample;
+
+public class Main {
+
+    public static void main(String[] args){
+        FirstExample.test();
+    }
+}
---
+++ src/com/lesliefish/test01firstexample/FirstExample.java
@@ -0,0 +1,54 @@
+package com.lesliefish.test01firstexample;
+
+import java.sql.*;
+
+public class FirstExample {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/EMP?serverTimezone=GMT%2B8";
+
+    static final String USER = "root";
+    static final String PWD = "    ";
+
+    public static void test() {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            // 注册jdbc驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 打开连接
+            System.out.println("Connecting to database...");
+            connection = DriverManager.getConnection(DB_URL, USER, PWD);
+
+            // 执行查询
+            System.out.println("Creating statement...");
+            statement = connection.createStatement();
+            String sql = "SELECT id, first, last, age FROM Employees";
+
+            ResultSet rs = statement.executeQuery(sql);
+            // 从查询结果中提取数据
+
+            while (rs.next()) {
+                // 通过列名获取数据
+                int id = rs.getInt("id");
+                int age = rs.getInt("age");
+                String first = rs.getString("first");
+                String last = rs.getString("last");
+
+                // 打印结果
+                System.out.print("ID: " + id);
+                System.out.print(", Age: " + age);
+                System.out.print(", First: " + first);
+                System.out.println(", Last: " + last);
+            }
+
+            // 关闭
+            rs.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
