adb4b511921c92ae5ef47ab6643dbf7033236ac2
leiyu <yuleics@outlook.com>
2019-03-11 09:41:53 +0800

    JDBC test sql date and time

--- src/com/lesliefish/Main.java
+++ src/com/lesliefish/Main.java
@@ -1,10 +1,12 @@
 package com.lesliefish;
 
 import com.lesliefish.test01firstexample.FirstExample;
+import com.lesliefish.test02datetime.DateTimeTester;
 
 public class Main {
 
     public static void main(String[] args){
-        FirstExample.test();
+        // FirstExample.test();
+        DateTimeTester.test();
     }
 }
---
+++ src/com/lesliefish/test02datetime/DateTimeTester.java
@@ -0,0 +1,22 @@
+package com.lesliefish.test02datetime;
+
+public class DateTimeTester {
+    public static void test() {
+        // util date
+        java.util.Date date = new java.util.Date();
+        long time = date.getTime();
+        System.out.println("the java date is :" + date.toString());
+
+        // sql date xxxx-xx-xx格式
+        java.sql.Date sqlDate = new java.sql.Date(time);
+        System.out.println("The SQL DATE is: " + sqlDate.toString());
+
+        // 获取打印 SQL TIME
+        java.sql.Time sqlTime = new java.sql.Time(time);
+        System.out.println("The SQL TIME is: " + sqlTime.toString());
+
+        // 获取打印 SQL TIMESTAMP 时间戳
+        java.sql.Timestamp sqlTimestamp = new java.sql.Timestamp(time);
+        System.out.println("The SQL TIMESTAMP is: " + sqlTimestamp.toString());
+    }
+}
